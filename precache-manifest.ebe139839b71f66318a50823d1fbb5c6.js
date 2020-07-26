e8f9076251f7c4924c2763cb7e3148bff69450f2
Risolat <utkurova.risolat@mail.ru>
2020-07-26 13:19:01 +0500

    Updates

---
+++ precache-manifest.ebe139839b71f66318a50823d1fbb5c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e63ac53683feb311e660e67485005e",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "0c9fc5b1c8884c6ee92b",
+    "url": "/youtube-clone/static/css/main.4600b240.chunk.css"
+  },
+  {
+    "revision": "0d95b2845cedd34d8a58",
+    "url": "/youtube-clone/static/js/2.dd272933.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-clone/static/js/2.dd272933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c9fc5b1c8884c6ee92b",
+    "url": "/youtube-clone/static/js/main.4cfe99ac.chunk.js"
+  },
+  {
+    "revision": "8a09cd451cb85b60110f",
+    "url": "/youtube-clone/static/js/runtime-main.a270c926.js"
+  }
+]);
